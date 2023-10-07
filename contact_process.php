37f6da00f73567d6c27ccca2ef1ded4f9a9efc47
Harish Sundar <hsundar@hsundarP9V9Q.vmware.com>
2023-10-07 11:47:03 +0530

    Contact PHP updated with SMTP

--- contact_process.php
+++ contact_process.php
@@ -1,27 +1,44 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php'; // Path to Composer's autoloader
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $toEmail = "harishsundarg@gmail.com"; // Replace with the recipient's email address
+    $toEmail = "recipient@example.com"; // Replace with the recipient's email address
     $subject = $_POST["subject"];
     $message = $_POST["message"];
     $name = $_POST["name"];
     $fromEmail = $_POST["email"];
 
-    $headers = "From: $name <$fromEmail>\r\n";
-    $headers .= "Reply-To: $fromEmail\r\n";
-    $headers .= "Content-type: text/html\r\n";
+    // Create a PHPMailer object
+    $mail = new PHPMailer(true);
 
-    // Check if all fields are filled
-    if (!empty($subject) && !empty($message) && !empty($name) && !empty($fromEmail)) {
-        // Attempt to send the email
-        if (mail($toEmail, $subject, $message, $headers)) {
-            echo "success"; // You can customize this success message as needed
-        } else {
-            echo "error"; // Display an error message if the email couldn't be sent
-        }
-    } else {
-        echo "empty"; // Display a message if any of the fields are empty
+    try {
+        //Server settings
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtpout.secureserver.net';                     // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'info@csaect.com';           // SMTP username
+        $mail->Password = 'Root$1234';               // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;                                    // TCP port to connect to
+
+        //Recipients
+        $mail->setFrom($fromEmail, $name);
+        $mail->addAddress($toEmail);                          // Add a recipient
+
+        //Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = $subject;
+        $mail->Body    = $message;
+
+        $mail->send();
+        echo "success";
+    } catch (Exception $e) {
+        echo "error";
     }
 } else {
-    echo "error"; // Handle invalid form submissions
+    echo "error";
 }
 ?>
