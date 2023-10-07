9faffbb69bb0409ea310da604e95919304dc1290
Harish Sundar <hsundar@hsundarP9V9Q.vmware.com>
2023-10-07 10:01:16 +0530

    Php email updated

--- contact_process.php
+++ contact_process.php
@@ -1,14 +1,27 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to = "harishsundarg@gmail.com"; // Change this to your email address
+    $toEmail = "harishsundarg@gmail.com"; // Replace with the recipient's email address
     $subject = $_POST["subject"];
     $message = $_POST["message"];
-    $headers = "From: " . $_POST["email"] . "\r\n";
-    
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Email sent successfully!";
+    $name = $_POST["name"];
+    $fromEmail = $_POST["email"];
+
+    $headers = "From: $name <$fromEmail>\r\n";
+    $headers .= "Reply-To: $fromEmail\r\n";
+    $headers .= "Content-type: text/html\r\n";
+
+    // Check if all fields are filled
+    if (!empty($subject) && !empty($message) && !empty($name) && !empty($fromEmail)) {
+        // Attempt to send the email
+        if (mail($toEmail, $subject, $message, $headers)) {
+            echo "success"; // You can customize this success message as needed
+        } else {
+            echo "error"; // Display an error message if the email couldn't be sent
+        }
     } else {
-        echo "Failed to send email.";
+        echo "empty"; // Display a message if any of the fields are empty
     }
+} else {
+    echo "error"; // Handle invalid form submissions
 }
 ?>
